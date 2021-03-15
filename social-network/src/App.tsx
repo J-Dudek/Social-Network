832696df134925165918677bafc5a3082a27e8df
Julien Dudek <julien.dudek@gmail.com>
2021-03-15 22:01:42 +0100

    ✨ add basic frontend

---
+++ social-network/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the M1IIIS2 project : Social Network
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
