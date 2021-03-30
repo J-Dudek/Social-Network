59d3f7b679f4bf69a2d0ee9c614260e264831e04
Julien Dudek <julien.dudek@gmail.com>
2021-03-30 14:29:39 +0200

    🧐 add dto

---
+++ friend/src/main/java/mjp/socialnetwork/friend/model/dto/UserDTO.java
@@ -0,0 +1,134 @@
+package mjp.socialnetwork.friend.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import mjp.socialnetwork.friend.views.UserViews;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class UserDTO {
+
+    @JsonView(UserViews.Private.class)
+    private Long idUser;
+    @JsonView(UserViews.Public.class)
+    private String firstName;
+    @JsonView(UserViews.Public.class)
+    private String lastName;
+    @JsonView(UserViews.Friends.class)
+    private Date birthdate;
+    @JsonView(UserViews.Friends.class)
+    private String email;
+    @JsonView(UserViews.Friends.class)
+    private String phoneNumber;
+    @JsonView(UserViews.Friends.class)
+    private String city;
+    @JsonView(UserViews.Friends.class)
+    private Timestamp signInDate;
+    @JsonView(UserViews.Friends.class)
+    private String username;
+    @JsonView(UserViews.Admin.class)
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long idUser, String firstName, String lastName, Date birthdate, String email, String phoneNumber, String city, Timestamp signInDate,String username, String password) {
+        this.idUser = idUser;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.signInDate = signInDate;
+        this.username = username;
+        this.password = password;
+    }
+    public UserDTO(String firstName, String lastName, Date birthdate, String email, String phoneNumber, String city, Timestamp signInDate, String username,String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.signInDate = signInDate;
+        this.username= username;
+        this.password = password;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Timestamp getSignInDate() {
+        return signInDate;
+    }
+
+    public void setSignInDate(Timestamp signInDate) {
+        this.signInDate = signInDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username;}
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ post/src/main/java/mjp/socialnetwork/post/model/dto/PostDTO.java
@@ -0,0 +1,70 @@
+package mjp.socialnetwork.post.model.dto;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class PostDTO {
+
+    private Long idPost;
+    private String message;
+    private Timestamp publicationDate;
+    private boolean isPublic;
+    private Long userId;
+
+    public PostDTO() {
+    }
+
+    public PostDTO(Long idPost, String message, Timestamp publicationDate, boolean isPublic, Long userId) {
+        this.idPost = idPost;
+        this.message = message;
+        this.publicationDate = publicationDate;
+        this.isPublic = isPublic;
+        this.userId = userId;
+    }
+    public PostDTO(String message, Timestamp publicationDate, boolean isPublic, Long userId) {
+        this.message = message;
+        this.publicationDate = publicationDate;
+        this.isPublic = isPublic;
+        this.userId = userId;
+    }
+
+    public Long getIdPost() {
+        return idPost;
+    }
+
+    public void setIdPost(Long idPost) {
+        this.idPost = idPost;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Timestamp publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
