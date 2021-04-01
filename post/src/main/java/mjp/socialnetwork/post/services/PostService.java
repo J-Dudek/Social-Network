d9cf384f38c14307279e7e9754ff4a52065ab606
Julien Dudek <julien.dudek@gmail.com>
2021-04-01 21:15:58 +0200

    💥 converter sur post app

--- post/src/main/java/mjp/socialnetwork/post/services/PostService.java
+++ post/src/main/java/mjp/socialnetwork/post/services/PostService.java
@@ -1,9 +1,10 @@
 package mjp.socialnetwork.post.services;
 
 import lombok.AllArgsConstructor;
-import mjp.socialnetwork.post.converters.PostConverter;
+import mjp.socialnetwork.post.model.Post;
 import mjp.socialnetwork.post.model.dto.PostDTO;
 import mjp.socialnetwork.post.repositories.PostRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
@@ -14,16 +15,15 @@ import reactor.core.publisher.Mono;
 @Service
 public class PostService {
 
-    private final PostConverter postConverter;
     private final PostRepository  postRepository;
+    private final ModelMapper modelMapper;
 
 
     public Flux<PostDTO> findAllPosts() {
-
-        return postRepository.findAll().map(post -> postConverter.entityToDto(post));
+        return postRepository.findAll().map(post -> modelMapper.map(post,PostDTO.class));
     }
 
     public Mono<PostDTO> createNewPost(PostDTO postDTO) {
-        return postRepository.save(postConverter.dtoToEntity(postDTO)).map(post -> postConverter.entityToDto(post));
+        return postRepository.save(modelMapper.map(postDTO, Post.class)).map(post -> modelMapper.map(post,PostDTO.class));
     }
 }
