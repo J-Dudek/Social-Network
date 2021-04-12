e6c40bcec6cf8b7b7e50422737f9e881de853d65
Julien Dudek <julien.dudek@gmail.com>
2021-04-12 12:10:09 +0200

    ♻️

--- friend/src/main/java/mjp/socialnetwork/friend/repositories/UserRepository.java
+++ friend/src/main/java/mjp/socialnetwork/friend/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+public interface UserRepository extends ReactiveCrudRepository<User, String> {
     @Query("SELECT * FROM user WHERE last_name = :lastname")
     Flux<User> findByLastName(String lastName);
 }
--- friend/src/main/java/mjp/socialnetwork/friend/services/UserService.java
+++ friend/src/main/java/mjp/socialnetwork/friend/services/UserService.java
@@ -1,6 +1,7 @@
 package mjp.socialnetwork.friend.services;
 
 import lombok.AllArgsConstructor;
+import mjp.socialnetwork.friend.model.User;
 import mjp.socialnetwork.friend.model.dto.UserDTO;
 import mjp.socialnetwork.friend.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
@@ -18,7 +19,26 @@ public class UserService {
     private final ModelMapper modelMapper;
 
     public Flux<UserDTO> findAllUsers() {
-         return userRepository.findAll().map(user -> modelMapper.map(user,UserDTO.class));
+         return userRepository.findAll().map(this::userToDTO);
     }
+
+    /**
+     * permet de convertir user en DTO
+     * @param user
+     * @return
+     */
+    private UserDTO userToDTO(User user){
+        return modelMapper.map(user,UserDTO.class);
+    }
+
+    /**
+     * permet de convertir DTO en user
+     * @param userDTO
+     * @return
+     */
+    private User dtoToUser(UserDTO userDTO){
+        return modelMapper.map(userDTO,User.class);
+    }
+
 
 }
