8f1fed3464ea87b5d71586a053d6bb01778cf423
Julien Dudek <julien.dudek@gmail.com>
2021-04-28 22:00:48 +0200

    #6 ✨add mapper Friendship

--- friend/src/main/java/mjp/socialnetwork/friend/services/FriendshipService.java
+++ friend/src/main/java/mjp/socialnetwork/friend/services/FriendshipService.java
@@ -1,6 +1,10 @@
 package mjp.socialnetwork.friend.services;
 
 import lombok.AllArgsConstructor;
+import mjp.socialnetwork.friend.model.Friendship;
+import mjp.socialnetwork.friend.model.dto.FriendshipDTO;
+import mjp.socialnetwork.friend.repositories.FriendshipRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,6 +12,28 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 @Service
 public class FriendshipService {
+
+    private final ModelMapper modelMapper;
+    private final FriendshipRepository friendshipRepository;
+
+
+    /**
+     * Mapper de friendship en friendshipDTO
+     * @param friendship
+     * @return
+     */
+    public FriendshipDTO userToDTO(Friendship friendship){
+        return modelMapper.map(friendship,FriendshipDTO.class);
+    }
+
+    /**
+     * mapper de friendshipDTO en friendship
+     * @param friendshipDTO
+     * @return
+     */
+    public Friendship dtoToUser(FriendshipDTO friendshipDTO){
+        return modelMapper.map(friendshipDTO,Friendship.class);
+    }
 
 
 }
