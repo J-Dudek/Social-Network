a0c3d50a90b573478cf346b4dc87577750b5aa25
Julien Dudek <julien.dudek@gmail.com>
2021-04-28 12:17:19 +0200

    ⚰️

--- friend/src/main/java/mjp/socialnetwork/friend/repositories/InvitationRepository.java
+++
@@ -1,7 +0,0 @@
-package mjp.socialnetwork.friend.repositories;
-
-import mjp.socialnetwork.friend.model.Invitation;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface InvitationRepository extends ReactiveCrudRepository<Invitation, Long> {
-}
