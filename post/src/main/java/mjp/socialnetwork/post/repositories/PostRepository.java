222f4837ad508d2808d85b81248f779c1694e464
Morgan Lombard <morgan.lombard@protonmail.com>
2021-04-29 12:37:35 +0200

    #10 maj post repository

--- post/src/main/java/mjp/socialnetwork/post/repositories/PostRepository.java
+++ post/src/main/java/mjp/socialnetwork/post/repositories/PostRepository.java
@@ -2,6 +2,9 @@ package mjp.socialnetwork.post.repositories;
 
 import mjp.socialnetwork.post.model.Post;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface PostRepository extends ReactiveCrudRepository<Post, Long> {
+
+    public Flux<Post> findPostByIdAndPublic(Long id, boolean isPublic);
 }
