2f0d4538f29ce8d11cc36bc7b7d624099d68ab42
Julien Dudek <julien.dudek@gmail.com>
2021-05-05 00:25:37 +0200

    🎨 ⚡️#17

--- social-network/src/components/header/profile.tsx
+++ social-network/src/components/header/profile.tsx
@@ -1,26 +1,65 @@
-import React from "react";
-import {Card, Icon, Image} from 'semantic-ui-react'
-
-
+import React, { useEffect, useState}from "react";
 import {useAuth0} from "@auth0/auth0-react";
+import { IUser } from "../../types/IUser";
+import axios from "axios";
+import { Card, Image, Icon } from "semantic-ui-react";
+import monkey from '../../images/monkeyInAsuit.jpg';
+
+const defaultUser: IUser = {};
 
 
 const Profile = () => {
-    const { user } = useAuth0();
-    const { name, picture, email } = user;
+  
+  const [user, setUser] = useState<IUser>(defaultUser);
+  
+  
+  const { getAccessTokenSilently } = useAuth0();
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+  useEffect(() => {
+    // Create an scoped async function in the hook
+    async function getInfos() {
+        const token = await getAccessTokenSilently();
+        axios
+    .get<IUser>(`${serverUrl}/friends/users/logOrsign`, {
+      
+        headers: {
+          Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }
+    })
+            .then((response) => {
+                console.log(response);
+                console.log("data");
+                console.log(response.data);
+      setUser(response.data);
+      
+    })
+    .catch((ex) => {
+      console.log(ex);
+      
+    });
+        
+    }
+          getInfos()
+      
+  }, [getAccessTokenSilently,serverUrl]);
 
     return (
         
-        <div className="ui centered card">
+         <div className="ui centered card">
             <Card>
-              <Image src={picture} wrapped ui={false} />
+              <Image src={monkey} wrapped ui={false} />
               <Card.Content>
-                <Card.Header>{name}</Card.Header>
+            <Card.Header >{ user.username}</Card.Header>
+            
                 <Card.Meta>
-                  <span className='date'>{email}</span>
+                  <div><Icon name="mail"/> {user.email} </div>
+                  <div><Icon name="phone"/> {user.phoneNumber} </div>
                 </Card.Meta>
                 <Card.Description>
-                  Garde t-on la desc?
+              <div>Hi, I'm {user.firstName}, I'm leave in {user.city}.</div>
+                  <div><Icon name="birthday cake" />I was born on {user.birthdate}.</div>
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
