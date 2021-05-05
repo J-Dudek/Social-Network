78fda8d58e20e5133f3a3215b44a80fc1f13118d
Julien Dudek <julien.dudek@gmail.com>
2021-05-05 21:04:35 +0200

    🚧   #3

--- friend/src/main/java/mjp/socialnetwork/friend/controllers/FriendshipController.java
+++ friend/src/main/java/mjp/socialnetwork/friend/controllers/FriendshipController.java
@@ -59,4 +59,37 @@ public class FriendshipController {
     public Mono<FriendshipDTO> acceptInvitation(Principal principal, @RequestBody Long idInvit){
         return friendshipService.acceptInvitation(principal,idInvit).map(friendshipService::userToDTO);
     }
+
+    /**
+     * Annulation d'une invitation envoyée.
+     * @param principal passé dans le token,corrsspond à celui qui a fait l'invitation
+     * @param idInvit l'invitation à annuler
+     * @return l'invitation supprimée.
+     */
+    @PutMapping(path="/cancel")
+    public Mono<FriendshipDTO> cancelInvitation(Principal principal,@RequestBody Long idInvit){
+        return friendshipService.cancelPendingInvitation(principal,idInvit).map(friendshipService::userToDTO);
+    }
+
+    /**
+     * Refu d'une invitation recus
+     * @param principal passé dans le token, correspond à l'user
+     * @param idInvit l'invitation concernée
+     * @return l'invitation supprimée
+     */
+    @DeleteMapping(path="/reject")
+    public Mono<FriendshipDTO> rejectInvitation(Principal principal, @RequestBody Long idInvit){
+        return friendshipService.cancelPendingInvitation(principal,idInvit).map(friendshipService::userToDTO);
+    }
+
+    /**
+     * Quand un amis supprime sa relation avec un autre
+     * @param principal l'user qui prend la decision de rompre
+     * @param secondUserId l'id du futur ex-ami
+     * @return la relation supprimée
+     */
+    @DeleteMapping(path="delete")
+    public Mono<FriendshipDTO> deleteRelation(Principal principal,@RequestBody String secondUserId){
+        return friendshipService.deleteRelation(principal,secondUserId).map(friendshipService::userToDTO);
+    }
 }
