392296995620886ee75a146c7565b38c8a4b0621
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-06 16:57:24 +0200

    maj user service

--- friend/src/main/java/mjp/socialnetwork/friend/controllers/UserController.java
+++ friend/src/main/java/mjp/socialnetwork/friend/controllers/UserController.java
@@ -2,14 +2,11 @@ package mjp.socialnetwork.friend.controllers;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
-import mjp.socialnetwork.friend.exceptions.errors.SocialNetworkException;
 import mjp.socialnetwork.friend.model.dto.UserDTO;
 import mjp.socialnetwork.friend.services.UserService;
 import mjp.socialnetwork.friend.views.UserViews;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,57 +17,44 @@ import java.security.Principal;
 @RequestMapping(path = "/friends/users")
 public class UserController {
 
-    @Autowired
     private final UserService userService;
-
-
 
     @GetMapping(path = "/all")
     public Flux<UserDTO> findAllUsers(Principal principal) {
-        try {
-            return this.userService.findAllUsers();
-        } catch (
-                SocialNetworkException exception) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+        return this.userService.findAllUsers();
     }
 
 
     @GetMapping(path = "/logOrsign")
     public Mono<UserDTO> logOrsign(Principal principal) {
-       return userService.logOrsign(principal).map(userService::userToDTO);
+        return userService.logOrsign(principal);
     }
 
     @GetMapping(path = "/{userId}")
     @JsonView(UserViews.Public.class)
-    public Mono<UserDTO> findUserByUserId(@PathVariable("userId") String userId){
-
-        return userService.findById(userId).map(userService::userToDTO);
+    public Mono<ResponseEntity<UserDTO>> findUserByUserId(@PathVariable("userId") String userId) {
+        return userService.findById(userId)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PutMapping(path = "/update")
-    public Mono<UserDTO> updateUser(Principal principal,@RequestBody UserDTO newUserDto) {
-        try {
-            return this.userService.updateUser(principal,newUserDto).map(userService::userToDTO);
-        } catch (SocialNetworkException exception) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+    public Mono<ResponseEntity<UserDTO>> updateUser(Principal principal, @RequestBody Mono<UserDTO> newUserDto) {
+        return this.userService.updateUser(principal, newUserDto)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping(path = "/delete/")
-    public void deleteUserById(Principal principal) {
-        try {
-            this.userService.deleteUserById(principal);
-        } catch (SocialNetworkException exception) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+    public Mono<Void> deleteUserById(Principal principal) {
+        return this.userService.deleteUserById(principal);
     }
 
     @GetMapping(path = "/{firstName}/{lastName}")
     @JsonView(UserViews.Public.class)
-    public Flux<UserDTO> findUsersByFirstNameOrLastName(@PathVariable("firstName") String firstName,@PathVariable("lastName") String lastName){
+    public Flux<UserDTO> findUsersByFirstNameOrLastName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
 
-        return userService.findByfirstOrlastNameLike(firstName,lastName).map(userService::userToDTO);
+        return userService.findByfirstOrlastNameLike(firstName, lastName);
     }
 
 
--- friend/src/main/java/mjp/socialnetwork/friend/services/UserService.java
+++ friend/src/main/java/mjp/socialnetwork/friend/services/UserService.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import mjp.socialnetwork.friend.model.User;
 import mjp.socialnetwork.friend.model.dto.UserDTO;
 import mjp.socialnetwork.friend.repositories.UserRepository;
-import org.modelmapper.ModelMapper;
+import mjp.socialnetwork.friend.utils.UserMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
@@ -20,115 +20,88 @@ public class UserService {
 
 
     private final UserRepository userRepository;
-    private final ModelMapper modelMapper;
 
     //TODO DELETE just for test
     public Flux<UserDTO> findAllUsers() {
-         return userRepository.findAll().map(this::userToDTO);
+        return userRepository.findAll().map(UserMapper::toDto);
     }
 
     /**
      * Recherche si l'utilisateur existe en base, si non, l'insert
+     *
      * @param principal
      * @return Mono user inséré ou trouvé en bdd
      */
     @Transactional
-    public Mono<User> logOrsign(Principal principal){
+    public Mono<UserDTO> logOrsign(Principal principal) {
         return userRepository.existsById(principal.getName())
-                .flatMap(aBoolean -> {
-                    if (!aBoolean) {
-                        User newUser = User.builder()
+                .flatMap(exists -> {
+                    if (Boolean.FALSE.equals(exists)) {
+                        return userRepository.save(User.builder()
                                 .id(principal.getName())
                                 .signInDate(LocalDateTime.now())
                                 .isNew(true) // for stats
                                 .newUser(true) //transcient
-                                .build();
-                        return userRepository.save(newUser);
+                                .build())
+                                .map(UserMapper::toDto);
                     } else {
-                        Mono<User> user = userRepository.findById(principal.getName());
-                        return user;
+                        return userRepository.findById(principal.getName())
+                                .map(UserMapper::toDto);
                     }
                 });
     }
 
     /**
      * Recuperation d'un utilisateur en passant son id technique en parametre
+     *
      * @param userId
      * @return
      */
-    public Mono<User> findById(String userId){
-        return userRepository.findById(userId);
+    public Mono<UserDTO> findById(String userId) {
+
+        return userRepository.findById(userId)
+                .map(UserMapper::toDto);
     }
 
     /**
-     *  Permet de mettre à jour un utilisateur
+     * Permet de mettre à jour un utilisateur
+     *
      * @param principal l'utilisateur connecte
-     * @param userDTO les données mise à jour
+     * @param userDTO   les données mise à jour
      * @return
      */
     @Transactional
-    public Mono<User> updateUser(Principal principal,UserDTO userDTO){
+    public Mono<UserDTO> updateUser(Principal principal, Mono<UserDTO> userDTO) {
 
         return userRepository
                 .findById(principal.getName())
-                .map(user -> {
-                    user.setFirstName(userDTO.getFirstName());
-                    user.setLastName(userDTO.getLastName());
-                    user.setBirthdate(userDTO.getBirthdate());
-                    user.setUsername(userDTO.getUsername());
-                    user.setEmail(userDTO.getEmail());
-                    user.setPhoneNumber(userDTO.getPhoneNumber());
-                    user.setCity(userDTO.getCity());
-                    user.setIsNew(false);
-                    return user;
-                })
-                .flatMap(this.userRepository::save);
-
+                .flatMap(user -> userDTO
+                        .map(UserMapper::toEntity)
+                        .doOnNext(u -> u.setIsNew(false))
+                )
+                .flatMap(this.userRepository::save)
+                .map(UserMapper::toDto);
     }
 
     /**
      * Permet de rechercher les utilisateur ayant un nom ou un prenom qui like
+     *
      * @param firstname
      * @param lastname
      * @return
      */
-    public Flux<User> findByfirstOrlastNameLike(String firstname, String lastname){
-        return userRepository.findUsersByFirstNameLikeOrLastNameLike(firstname,lastname);
+    public Flux<UserDTO> findByfirstOrlastNameLike(String firstname, String lastname) {
+        return userRepository.findUsersByFirstNameLikeOrLastNameLike(firstname, lastname)
+                .map(UserMapper::toDto);
     }
 
     /**
      * Suppression de l'user qui fait appel à la methode.
+     *
      * @param principal
      */
-    public void deleteUserById(Principal principal){
-        userRepository.deleteById(principal.getName());
-    }
-
-    /**
-     * permet de convertir user en DTO
-     * @param user
-     * @return
-     */
-    public UserDTO userToDTO(User user){
-        return modelMapper.map(user,UserDTO.class);
-    }
-
-    /**
-     * permet de convertir DTO en user
-     * @param userDTO
-     * @return
-     */
-    public User dtoToUser(UserDTO userDTO){
-        return User.builder()
-                .id(userDTO.getIdUser())
-                .city(userDTO.getCity())
-                .email(userDTO.getEmail())
-                .firstName(userDTO.getFirstName())
-                .lastName(userDTO.getLastName())
-                .phoneNumber(userDTO.getPhoneNumber())
-                .birthdate(userDTO.getBirthdate().plusDays(1))
-        .build();
-
+    public Mono<Void> deleteUserById(Principal principal) {
+        return userRepository.deleteById(principal.getName());
     }
 
 
