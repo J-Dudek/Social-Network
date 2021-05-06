40c25be8cd892656a25bd799e01a14060cdf82bb
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-06 21:32:26 +0200

    maj userMapper + new PostMapper

--- friend/src/main/java/mjp/socialnetwork/friend/utils/UserMapper.java
+++ friend/src/main/java/mjp/socialnetwork/friend/utils/UserMapper.java
@@ -8,23 +8,27 @@ public class UserMapper {
 
     private static final ModelMapper modelMapper = new ModelMapper();
 
-    private UserMapper() { throw new IllegalStateException("Utility class");}
+    private UserMapper() {
+        throw new IllegalStateException("Utility class");
+    }
 
     /**
      * permet de convertir user en DTO
+     *
      * @param user
      * @return USerDTO
      */
-    public static UserDTO toDto(User user){
-        return modelMapper.map(user,UserDTO.class);
+    public static UserDTO toDto(User user) {
+        return modelMapper.map(user, UserDTO.class);
     }
 
     /**
      * permet de convertir DTO en user
+     *
      * @param userDTO
      * @return UserDTO
      */
-    public static User toEntity(UserDTO userDTO){
+    public static User toEntity(UserDTO userDTO) {
         return User.builder()
                 .id(userDTO.getIdUser())
                 .city(userDTO.getCity())
