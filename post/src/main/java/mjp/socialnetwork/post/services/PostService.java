f8b8e542c3f6f4fed0be3a3bbe10644a1bbefa7f
Morgan Lombard <morgan.lombard@protonmail.com>
2021-05-06 22:43:15 +0200

    New endpoints for PostController

--- post/src/main/java/mjp/socialnetwork/post/services/PostService.java
+++ post/src/main/java/mjp/socialnetwork/post/services/PostService.java
@@ -1,17 +1,16 @@
 package mjp.socialnetwork.post.services;
 
 import lombok.AllArgsConstructor;
-import mjp.socialnetwork.post.model.Post;
+import mjp.socialnetwork.post.exceptions.PostException;
 import mjp.socialnetwork.post.model.dto.PostDTO;
 import mjp.socialnetwork.post.repositories.PostRepository;
-import org.modelmapper.ModelMapper;
+import mjp.socialnetwork.post.utils.PostMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.security.Principal;
 
 @Transactional
 @AllArgsConstructor
@@ -19,57 +18,49 @@ import java.time.LocalDateTime;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final ModelMapper modelMapper;
 
-    //TODO DELETE just for test
-    public Flux<PostDTO> findAllPosts() { return postRepository.findAll().map(this::postToDTO); }
-
-
-    public Flux<Post> findAll() {
-        return postRepository.findAll();
+    public Flux<PostDTO> findAll() {
+        return this.postRepository.findAll()
+                .map(PostMapper::toDto);
     }
 
-    public Flux<Post> findAllPrivate(long id, boolean isPublic) {
-        return this.postRepository.findPostByIdAndPublic(id, isPublic);
+    public Flux<PostDTO> findAllPrivate(long id) {
+        return this.postRepository.findPostByIdAndPublic(id, false)
+                .map(PostMapper::toDto);
     }
 
-    public Mono<Post> findById(Long id) {
-        return this.postRepository.findById(id);
+    public Mono<PostDTO> findById(Long id) {
+        return this.postRepository.findById(id).map(PostMapper::toDto);
     }
 
-    public Mono<Post> delete(Long id) {
+    public Mono<PostDTO> create(Mono<PostDTO> postDTO) {
+        return postDTO
+                .map(PostMapper::toEntity)
+                .flatMap(this.postRepository::save)
+                .map(PostMapper::toDto);
+    }
+
+    public Mono<PostDTO> update(Long id, Mono<PostDTO> postDTOMono) {
+        return this.postRepository.findById(id)
+                .flatMap(post -> postDTOMono
+                        .map(PostMapper::toEntity)
+                        .doOnNext(p -> p.setId(id)))
+                .flatMap(this.postRepository::save)
+                .map(PostMapper::toDto);
+    }
+
+    public Mono<Void> delete(Principal principal, Long id) {
         return this.postRepository
                 .findById(id)
                 .flatMap(
-                        post -> this.postRepository
-                                .deleteById(post.getId())
-                                .thenReturn(post)
+                        post -> {
+                            if (post.getUserId().equals(principal.getName())) {
+                                return this.postRepository.deleteById(id);
+                            } else {
+                                return Mono.error(PostException::new);
+                            }
+                        }
                 );
-    }
-
-    public Mono<Post> update(Post newPost) {
-        return this.postRepository
-                .findById(newPost.getId())
-                .map(oldPost -> {
-                    oldPost.setMessage(newPost.getMessage());
-                    oldPost.setPublic(newPost.isPublic());
-                    return oldPost;
-                })
-                .flatMap(this.postRepository::save);
-    }
-
-    public Mono<Post> create(Post post) {
-        return this.postRepository
-                .save(new Post(null, post.getMessage(), Timestamp.valueOf(LocalDateTime.now()), post.isPublic(), post.getUserId()));
-    }
-
-    /**
-     * permet de convertir user en DTO
-     * @param post
-     * @return
-     */
-    public PostDTO postToDTO(Post post){
-        return modelMapper.map(post,PostDTO.class);
     }
 
 
