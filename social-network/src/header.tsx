94beab791c391371c49b13b44d3b00d79fa7c00a
Julien Dudek <julien.dudek@gmail.com>
2021-05-10 10:15:48 +0200

    🐛

--- social-network/src/header.tsx
+++ social-network/src/header.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import AuthenticationButton from './components/header/authentication-button'
 import Logo from './components/header/logo'
-import {Header, Segment} from 'semantic-ui-react'
+import { Header, Segment } from 'semantic-ui-react'
 import CreatePost from './components/fonctionnals/createPost'
+import { useAuth0 } from "@auth0/auth0-react";
 
-const HeaderComponent = () => (
+
+const HeaderComponent = () => {
+  const { isAuthenticated } = useAuth0();
+
+
+  return (
     <Segment clearing>
       <Header as='h2' floated='right'>
-        <AuthenticationButton />           
+        <AuthenticationButton />
       </Header>
 
       <Header as='h2' floated='left'>
         <Logo />
-        <CreatePost  />        
+        {isAuthenticated && (<CreatePost />)}
+
       </Header>
     </Segment>
   )
-  
+
+}
+
 export default HeaderComponent
